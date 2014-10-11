4f8cb2b792073bc468344052541c0a9ca25e184a
John Funnell <john@parabolaresearch.com>
2014-10-11 11:47:39 +0100

    wip

--- src/lib/hevcasm.c
+++ src/lib/hevcasm.c
@@ -166,16 +166,16 @@ int hevcasm_main(int argc, const char *argv[])
 
 	int error_count = 0;
 
-	//hevcasm_test_quantize_inverse(&error_count, mask);
-	//hevcasm_test_quantize(&error_count, mask);
-	//hevcasm_test_quantize_reconstruct(&error_count, mask);
-	//hevcasm_test_ssd(&error_count, mask);
-	//hevcasm_test_sad(&error_count, mask);
-	//hevcasm_test_sad_multiref(&error_count, mask);
-	//hevcasm_test_pred_uni(&error_count, mask);
-	//hevcasm_test_pred_bi(&error_count, mask);
+	hevcasm_test_quantize_inverse(&error_count, mask);
+	hevcasm_test_quantize(&error_count, mask);
+	hevcasm_test_quantize_reconstruct(&error_count, mask);
+	hevcasm_test_ssd(&error_count, mask);
+	hevcasm_test_sad(&error_count, mask);
+	hevcasm_test_sad_multiref(&error_count, mask);
+	hevcasm_test_pred_uni(&error_count, mask);
+	hevcasm_test_pred_bi(&error_count, mask);
 	hevcasm_test_inverse_transform_add(&error_count, mask);
-	//hevcasm_test_transform(&error_count, mask);
+	hevcasm_test_transform(&error_count, mask);
 
 	printf("\n");
 	printf("HEVCasm self test: %d errors\n", error_count);
